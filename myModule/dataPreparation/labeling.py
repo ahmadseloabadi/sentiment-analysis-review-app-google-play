b69e8704de37162a684a15828cb5d3a5cfe82096
selo <ahmadseloabadi@gmail.com>
2025-03-30 22:59:45 +0700

    feat : add pelabelan text blob and inset lexicon

--- myModule/dataPreparation/labeling.py
+++ myModule/dataPreparation/labeling.py
@@ -4,6 +4,9 @@ import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from deep_translator import GoogleTranslator
 from myModule.reusable.downloadButton import download_data
+from textblob import TextBlob
+import csv
+
 def translate_to_english(dataset):
     translator = GoogleTranslator(source='auto', target='en')
     dataset['Stopword Removal'] = dataset['Stopword Removal'].astype(str)
@@ -79,7 +82,80 @@ def vader_labeling(dataset):
     download_data(data,"pelabelan vader")
 
 def textblob_labeling(dataset):
-    print("vader labeling")
+
+    # mengambil nilai subjectivity
+    def getSubjectivity(text):
+        return TextBlob(text).sentiment.subjectivity
+    # mengambil nilai polarity
+    def getPolarity(text):
+        return TextBlob(text).sentiment.polarity
+    # Melakukan Labelling
+    def getSentiment(score):
+        if score < 0 :
+            return 'negatif'
+        elif score == 0 :
+            return 'netral'
+        else :
+            return 'positif'
+        
+    data = translate_to_english(dataset)
+    data['English_Tweet'] = data['English_Tweet'].astype(str)
+
+    data['Subjectivity'] = data['English_Tweet'].apply(getSubjectivity)
+    data['Polarity'] = data['English_Tweet'].apply(getPolarity)
+    data['Sentiment'] = data['Polarity'].apply(getSentiment)
+
+    data=data.drop(columns='English_Tweet')
+    st.toast("berhasil melakukan pelabelan data", icon='🎉')
+    st.subheader('berikut merupakan tampilan dari pelabelan textblob')
+    st.dataframe(data)
+    download_data(data,"pelabelan textblob")
+
 
 def inset_labeling(dataset):
-    print("vader labeling")
+    lexicon_positive = dict()
+    with open('data/kamus/positive.tsv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+        for row in reader:
+            if "word" not in row and "weight" not in row:
+                lexicon_positive[row[0]] = int(row[1])
+
+    lexicon_negative = dict()
+    with open('data/kamus/negative.tsv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+        for row in reader:
+            if "word" not in row and "weight" not in row:
+                lexicon_negative[row[0]] = int(row[1])
+
+    def inset_lexicon(text):
+        word_scores = {}
+        score= 0
+        for word in text:
+            if word in lexicon_positive:
+                score += lexicon_positive[word]
+                word_scores[word] = lexicon_positive[word]
+            if word in lexicon_negative:
+                score += lexicon_negative[word]
+                word_scores[word] =lexicon_negative[word]
+            if (word in lexicon_negative) and (word in lexicon_positive):
+                word_scores[word] = lexicon_negative[word] + lexicon_positive[word]
+        polarity=''
+        if (score > 0):
+            polarity = 'positif'
+        elif(score<0):
+            polarity = 'negatif'
+        else:
+            polarity = 'netral'
+        return word_scores, score, polarity
+    
+    results = dataset.apply(inset_lexicon)
+    results = list(zip(*results))
+    # Menambahkan kolom baru ke DataFrame
+    dataset['score_term'] = results[0]
+    dataset['polarity_score'] = results[1]
+    dataset['sentiment'] = results[2]
+    dataset=dataset[['content','score_term','polarity_score','sentiment']]
+    st.toast("berhasil melakukan pelabelan data", icon='🎉')
+    st.subheader('berikut merupakan tampilan dari pelabelan inset lexicon')
+    st.dataframe(dataset)
+    download_data(dataset,"pelabelan inset lexicon")
